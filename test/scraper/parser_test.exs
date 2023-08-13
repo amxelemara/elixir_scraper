aadc430e8e0a7fe4a1c32ab6b944a3d3e6538de6
Am <amxelemara@gmail.com>
2023-08-13 23:56:10 +0100

    Test parser can extract relative links

---
+++ test/scraper/parser_test.exs
@@ -0,0 +1,13 @@
+defmodule ParserTest do
+  use ExUnit.Case 
+
+  test "parse relative links out of html" do
+    html = TestServer.render_page()
+    paths = Parser.get_paths(html)
+    assert not Enum.member?(paths, "http://www.google.com") 
+    assert ["about/"] == paths
+  end
+
+  
+  
+end

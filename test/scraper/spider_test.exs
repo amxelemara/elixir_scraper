809aa2eb69e9a179086a00e675ef710e117ac145
Am <amxelemara@gmail.com>
2023-08-13 21:42:48 +0100

    Implement a TestServer using Plug

--- test/scraper/spider_test.exs
+++ test/scraper/spider_test.exs
@@ -1,13 +1,45 @@
 defmodule SpiderTest do
   use ExUnit.Case
 
+  setup do
+    webserver = {Plug.Cowboy, plug: TestServer, scheme: :http, options: [port: 4000]}
+    {:ok, _} = Supervisor.start_link([webserver], strategy: :one_for_one)
+    :ok
+  end
+
   test "fetch a web page" do
-    url = "localhost:4000/"
+    url = "localhost:4000"
     {:ok, spider} = Spider.start_link(url)
-    {status, _} = Spider.scrape(spider, "/")
-    # ideally get a :http200 but need to implement
-    # a test server to call
-    assert :error  == status
+    {status, resp} = Spider.scrape(spider, "/")
+    assert {:ok, 200} == {status , resp.status_code}
   end
   
+end
+
+defmodule TestServer do
+  @moduledoc """
+    A dummy server to test scraping against
+    """
+  import Plug.Conn
+  
+  def init(options) do
+    options
+  end
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, render_page())
+  end
+
+  def render_page() do
+    """
+    <head>
+      <title> Dummy Site </title>
+    </head>
+    <body>
+      <h1> Welcome to dummy site </h1>
+    </body>
+    """
+  end
 end

5e954cde902d137efb963cb22bd115700d15f336
Am <amxelemara@gmail.com>
2023-08-14 19:01:19 +0100

    Test to scrape multiple pages in async
    Also move out Testserver from setup &
    start Spider in setup.

--- test/scraper/spider_test.exs
+++ test/scraper/spider_test.exs
@@ -2,16 +2,28 @@ defmodule SpiderTest do
   use ExUnit.Case
 
   setup do
-    webserver = {Plug.Cowboy, plug: TestServer, scheme: :http, options: [port: 4000]}
-    {:ok, _} = Supervisor.start_link([webserver], strategy: :one_for_one)
-    :ok
+    port = 4016
+    url = "localhost:#{port}"
+    {:ok, spider} = Spider.start_link(url)
+    %{spider: spider}
   end
 
-  test "fetch a web page" do
-    url = "localhost:4000"
-    {:ok, spider} = Spider.start_link(url)
+  test "fetch a web page", %{spider: spider} do
     {status, resp} = Spider.scrape(spider, "/")
     assert {:ok, 200} == {status , resp.status_code}
   end
-  
+
+  test "scrape multiple pages and message", %{spider: spider} do
+    paths = ["about/", "shop/", "profile/"]
+
+    results =
+      Spider.crawl_async(spider, paths)
+      |> Enum.map( &(Task.await(&1)) )
+
+    assert length(results) == 3
+    Enum.each(results, fn {status, resp} ->
+      assert {:ok, 200} == {status , resp.status_code}
+      end
+    )
+  end
 end

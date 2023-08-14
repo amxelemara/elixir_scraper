c607d781fdc165b93d03a0f8a3370a7f39ddc86e
Am <amxelemara@gmail.com>
2023-08-14 18:59:52 +0100

    TestServer is now started once under  Supervisor

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,5 @@
 ExUnit.start()
+
+port = 4016
+webserver = {Plug.Cowboy, plug: TestServer, scheme: :http, options: [port: port]}
+{:ok, _} = Supervisor.start_link([webserver], strategy: :one_for_one, name: TestServer)
